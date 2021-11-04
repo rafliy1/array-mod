caf8cdcbbdd16b89c0bf409d71b8c2906825c328
rafliy1 <muhamadrafliy88@gmail.com>
2021-11-04 16:20:06 +0700

    function barisan
    funciton barisan atau array

---
+++ barisan.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nama [4]string
+	nama[0] = "burhan"
+	nama[1] = "budi"
+	nama[2] = "ito"
+	nama[3] = "rudi"
+
+	fmt.Println(nama[0], nama[1], nama[2], nama[3])
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rafliy1/array-mod
+
+go 1.17
